c35b7e00bf5e01e98fd48a118c7dbd365d208382
Andy Tang <andy.tang@qp.me>
2022-07-06 16:21:49 -0700

    check if next extension is gif

--- src/GetOrCreateImage/GetOrCreateImage.js
+++ src/GetOrCreateImage/GetOrCreateImage.js
@@ -71,7 +71,7 @@ const GetOrCreateImage = async event => {
         console.info("key\n" + key)
       }
 
-      let isAnimated = (imageObj.Metadata["content-type"] == "image/gif") ? true: false;
+      let isAnimated = (nextExtension == "image/gif") ? true: false;
 
       // Required try/catch because Sharp.catch() doesn't seem to actually catch anything. 
       try {
